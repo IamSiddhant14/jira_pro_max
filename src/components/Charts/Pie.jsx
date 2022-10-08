248b9cbb113b0eda348209bf28f8461875f9719c
Siddhant <siddhantsharma669@gmail.com>
2022-10-08 13:57:32 +0530

    context api , sidebar done

---
+++ src/components/Charts/Pie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pie = () => {
+  return (
+    <div>
+      pie
+    </div>
+  )
+}
+
+export default Pie
+
---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChartsHeader = () => {
+  return (
+    <div>
+      ChartsHeade
+    </div>
+  )
+}
+
+export default ChartsHeader
